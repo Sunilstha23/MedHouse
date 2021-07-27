0419b75984fb50e7fa594b31616e95bfc47b4f3a
Sunil Shrestha <sunilshresthamail@gmail.com>
2021-07-27 08:47:14 +0545

    wcag tested except sigin, add &update img label error

--- customer/forms.py
+++ customer/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth import authenticate, get_user_model
+from django.forms import widgets
 from .models import Customer, User, CustomerImage
 
 user = get_user_model()
